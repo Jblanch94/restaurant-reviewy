6dccdd9609950b5350394f115f73999aa0863c6c
Jblanch94 <johnnyblanchard94@gmail.com>
2020-11-23 09:10:20 -0500

    Fixed typescript dependency issue

--- client/src/components/ui/ReviewCard.tsx
+++ client/src/components/ui/ReviewCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Card, Typography, CardContent, Container } from '@material-ui/core';
 import { Rating } from '@material-ui/lab';
 
